ce80489a4bc1c88f9685c83d26df5351c8a682a9
fayez <fayez@DESKTOP-EUG1F9J>
2021-01-17 00:13:38 +0300

    adding viewmodels

---
+++ Domain/Domain/ViewModels/SalesProducts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.ViewModels
+{
+    public class SalesProducts
+    {
+        public int Id { get; set; }
+        public string ArabicName { get; set; }
+        public string EnglishName { get; set; }
+        public double Cost { get; set; }
+        public int MyProperty { get; set; }
+    }
+}

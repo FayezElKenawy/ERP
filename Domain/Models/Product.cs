9d0a09eea70d38b89d920b540a2ee37656140a4c
Fayez Emad Elkenawy <fezoelkenawy24@yahoo.com>
2021-01-16 12:28:04 +0300

    adding modeles and repos

---
+++ Domain/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ArabicName { get; set; }
+        public string EnglishName { get; set; }
+        public string Model { get; set; }
+        public string Desc { get; set; }
+        public double Cost { get; set; }
+        public double SalePrice { get; set; }
+
+    }
+}

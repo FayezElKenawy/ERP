aad8fd24d1fcf43c150b1ed3109b68f83d501588
Fayez Emad Elkenawy <fezoelkenawy24@yahoo.com>
2021-02-03 12:13:55 +0300

    invoices

--- PresentationLayer/ERP/Startup.cs
+++ PresentationLayer/ERP/Startup.cs
@@ -68,7 +68,14 @@ namespace ERP
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                OnPrepareResponse = context =>
+                {
+                    context.Context.Response.Headers.Add("Cache-Control", "no-cache, no-store");
+                    context.Context.Response.Headers.Add("Expires", "-1");
+                }
+            });
 
             app.UseRouting();
 

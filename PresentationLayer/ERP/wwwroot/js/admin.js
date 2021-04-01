d020903ca04b6c202bb9af751652098e83dcca27
fayez <fayezelkenawy@gmail.com>
2021-04-01 18:08:00 +0300

    reAvtivate deleted user

--- PresentationLayer/ERP/wwwroot/js/admin.js
+++ PresentationLayer/ERP/wwwroot/js/admin.js
@@ -1,7 +1,7 @@
 ﻿/*remove user by ajax*/
 
     $('#delete').on('click', function (e) {
-            var operation = [{"path": "/isdeleted", "op": "replace", "value": "false" }];
+            var operation = [{"path": "/isdeleted", "op": "replace", "value": "true" }];
             var userId = $('#userid').attr("user");
         $.ajax({
             type: "patch",
@@ -11,5 +11,20 @@
             success: console.log(operation)
         });
 
-        });
+        
+    });
+
+
+
+$('#reActive').on('click', function () {
+    var operation = [{ "path": "/isdeleted", "op": "replace", "value": "false" }];
+    var userId = $(this).attr("user");
+    $.ajax({
+        type: "patch",
+        url: "api/users/" + userId,
+        contentType: "application/json",
+        data: JSON.stringify(operation),
+        success: console.log(operation)
+    });
+});
 
